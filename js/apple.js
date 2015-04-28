3cedc3a5ba40de30915b9b1473070b28b70a3a93
John Mahowald <john.mahowald@gmail.com>
2015-04-27 20:19:11 -0700

    apple selets a position based on board size

--- js/apple.js
+++ js/apple.js
@@ -1,15 +1,14 @@
 window.App = window.App || {}
 
-var Apple = App.Apple = function (game) {
-  this.game = game;
+var Apple = App.Apple = function (size) {
+  this.pos = this.startPos(size);
 }
 
-Apple.prototype.startPos = function () {
-  var len = this.game.board.rows;
+Apple.prototype.startPos = function (size) {
   var pos = [];
 
-  pos[0] = Math.floor(Math.random(len));
-  pos[1] = Math.floor(Math.random(len));
+  pos[0] = Math.floor(Math.random() * 10);
+  pos[1] = Math.floor(Math.random() * 10);
 
   return pos;
 }
