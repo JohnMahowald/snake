3dea90c509e15bf028b5592fe3884f70a8f9a763
John Mahowald <john.mahowald@gmail.com>
2015-04-27 20:19:38 -0700

    game can create and render apples

--- js/game.js
+++ js/game.js
@@ -6,7 +6,6 @@ var Game = App.Game = function (mount) {
   this.apples = []
   this.board = new App.Board(this.size);
   this.snake = new App.Snake(this.size);
-  this.apple = new App.Apple(this);
 }
 
 Game.prototype.start = function () {
@@ -14,19 +13,42 @@ Game.prototype.start = function () {
 
   setInterval( function () {
     this.advanceGame();
-  }.bind(this), 300);
+  }.bind(this), 600);
 
   setInterval( function () {
     this.snake.extend();
-  }.bind(this), 2000);
+  }.bind(this), 4000);
+
+  setInterval( function () {
+    this.addApple();
+  }.bind(this), 1000);
 }
 
 Game.prototype.advanceGame = function () {
   this.board.clearBoard();
   this.snake.advance();
   this.snake.render();
+  this.renderApples();
 }
 
 Game.prototype.renderBoard = function () {
   this.el.appendChild(this.board.board);
+}
+
+Game.prototype.addApple = function () {
+  this.apples.push(new App.Apple(this.size));
+}
+
+Game.prototype.renderApples = function () {
+  for (var i = 0; i < this.apples.length; i++) {
+    this._renderPos(this.apples[i].pos);
+  }
+}
+
+// Private
+
+Game.prototype._renderPos = function (pos, className) {
+  var row = document.getElementById(pos[0]);
+  var square = row.getElementsByClassName("col-" + pos[1])[0];
+  square.classList.add("apple");
 }
